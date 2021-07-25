66472d949dfa4edca2b94c0acfb52c43bc7f3633
Rafael Simor <rafaelsimor@gmail.com>
2021-07-25 03:12:50 +0100

    add proper search - 1st step

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,22 +1,56 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
-    var testButton = document.getElementById('testButton');
-    setupTestButtonEvents(testButton);
+    let elementsArray = document.querySelectorAll(".btn-build");
+    
+    elementsArray.forEach(function (elem) {
+        onBuildClick(elem);
+    });
 }, false);
 
-function setupTestButtonEvents(testButton) {
-    testButton.addEventListener("click", async () => {
+function onBuildClick(btn) {
+    btn.addEventListener("click", async () => {
         let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
         if (!isCorrectUrl(tab.url)) {
-            executeAlert(tab.id, invalidUrlAlert);
+            executeFunction(tab.id, invalidUrlAlert);
         }
         else {
-            executeAlert(tab.id, validUrlAlert);
+            executeFunction(tab.id, searchBuild);
+            window.close();
         }
     }, false);
 }
 
-function executeAlert(tabId, functionRef) {
+function searchBuild() {
+    document.getElementsByClassName("tab")[1].click();
+    let searchInput = document.querySelector('[placeholder="Search parts and abilities"]');
+
+    setTimeout(() => {
+        searchInput.focus();
+        contains("h6.text-gray-1", "Tail Slap")[0].click();
+        contains("h6.text-gray-1", "Swift Escape")[0].click();
+        contains("h6.text-gray-1", "Star Shuriken")[1].click();
+        contains("h6.text-gray-1", "Fish Hook")[0].click();
+
+        searchInput.blur();
+    }, 500);
+
+    let f = function contains(selector, text) {
+        var elements = document.querySelectorAll(selector);
+        return [].filter.call(elements, function (element) {
+            return RegExp('/^' + text + '$/').test(element.textContent);
+        });
+    }
+
+    function contains(selector, text) {
+        var elements = document.querySelectorAll(selector);
+        return [].filter.call(elements, function (element) {
+            return RegExp(text).test(element.textContent);
+        });
+    }
+}
+
+// Checks for invalid websites
+function executeFunction(tabId, functionRef) {
     chrome.scripting.executeScript({
         target: { tabId: tabId, allFrames: true },
         function: functionRef
@@ -29,8 +63,4 @@ function isCorrectUrl(url) {
 
 function invalidUrlAlert() {
     alert("Only use this extension on Axie Marketplace!");
-}
-
-function validUrlAlert() {
-    alert("Axie Marketplace!");
 }
